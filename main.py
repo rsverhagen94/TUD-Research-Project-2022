0fabbba6cf01044dc11cd5b7ac91b10b5c3f3669
Ruben <r.s.verhagen@tudelft.nl>
2024-03-18 18:35:14 +0100

    implement output logger

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
             print("Starting custom visualizer")
             vis_thread = visualization_server.run_matrx_visualizer(verbose=False, media_folder=media_folder)
 
-            robots = ['Brutus'] * 2 + ['Titus'] * 2
-            tasks = [1, 2, 5, 6]
+            robots = ['Brutus'] * 2 + ['Brutus'] * 2
+            tasks = [2, 2, 2, 2]
             random.shuffle(robots)
             random.shuffle(tasks)
             print(robots, tasks)
@@ -39,9 +39,9 @@ if __name__ == "__main__":
                 builder.api_info['matrx_paused'] = True
                 world.run(builder.api_info)
 
-                if choice1=="experiment":
+                if choice2=="experiment":
                     fld = os.getcwd()
-                    recent_dir = max(glob.glob(os.path.join(fld, '*/')), key=os.path.getmtime)
+                    recent_dir = max(glob.glob(os.path.join(fld, '*/'+choice1+'/')), key=os.path.getmtime)
                     recent_dir = max(glob.glob(os.path.join(recent_dir, '*/')), key=os.path.getmtime)
                     action_file = glob.glob(os.path.join(recent_dir,'world_1/action*'))[0]
                     message_file = glob.glob(os.path.join(recent_dir,'world_1/message*'))[0]
@@ -50,27 +50,14 @@ if __name__ == "__main__":
                     message_header = []
                     message_contents=[]
                     unique_agent_moves = []
-                    unique_human_moves = []
-                    human_moves = []
-                    idle_agent = 0
-                    idle_human = 0
-                    idle_together = 0
                     with open(action_file) as csvfile:
                         reader = csv.reader(csvfile, delimiter=';', quotechar="'")
                         for row in reader:
                             if action_header==[]:
                                 action_header=row
                                 continue
-                            if row[2:4] not in unique_agent_moves:
-                                unique_agent_moves.append(row[2:4])
-                            if row[4:6] not in unique_human_moves:
-                                unique_human_moves.append(row[4:6])
-                            if row[2] == "" and row[4]!='RemoveObjectTogether':
-                                idle_agent+=1
-                            if row[4] == "":
-                                idle_human+=1
-                            if row[2] == "" and row[4]=="":
-                                idle_together+=1
+                            if row[1:3] not in unique_agent_moves:
+                                unique_agent_moves.append(row[1:3])
                             res = {action_header[i]: row[i] for i in range(len(action_header))}
                             action_contents.append(res)
                     
@@ -85,16 +72,19 @@ if __name__ == "__main__":
 
                     no_messages_human = message_contents[-1]['total_number_messages_human']
                     no_messages_agent = message_contents[-1]['total_number_messages_agent']
+                    total_allocations = message_contents[-1]['total_allocations']
+                    human_allocations = message_contents[-1]['total_allocations_human']
+                    human_interventions = message_contents[-1]['total_interventions']
+                    mean_intervention_sensitivity = message_contents[-1]['mean_intervention_sensitivity']
+                    disagreement_ratio = int(human_interventions) / (int(total_allocations) - int(human_allocations))
                     no_ticks = action_contents[-1]['tick_nr']
-                    score = action_contents[-1]['score']
                     completeness = action_contents[-1]['completeness']
-                    ignored = message_contents[-1]['ignored']
 
                     print("Saving output...")
                     with open(os.path.join(recent_dir,'world_1/output.csv'),mode='w') as csv_file:
                         csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                        csv_writer.writerow(['completeness','score','no_ticks','ignored_suggestions','agent_moves','human_moves','agent_messages','human_messages','agent_idle','human_idle','simultaneous_idle'])
-                        csv_writer.writerow([completeness,score,no_ticks,ignored,len(unique_agent_moves),len(unique_human_moves),no_messages_agent,no_messages_human,idle_agent,idle_human,idle_together])
+                        csv_writer.writerow(['completeness','no_ticks','agent_moves','agent_messages','human_messages','allocations','human_allocations','human_interventions', 'disagreement','intervention_sensitivity'])
+                        csv_writer.writerow([completeness,no_ticks,len(unique_agent_moves),no_messages_agent,no_messages_human,total_allocations,human_allocations,human_interventions,disagreement_ratio,mean_intervention_sensitivity])
             print("DONE!")
             print("Shutting down custom visualizer")
             r = requests.get("http://localhost:" + str(visualization_server.port) + "/shutdown_visualizer")
